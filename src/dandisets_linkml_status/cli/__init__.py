7b0e86c898f8e6461a9c98fd651b8c8ce0c5c949
Isaac To <isaac.chun.to@gmail.com>
2024-08-12 15:57:46 -0700

    Specify CLI options

--- src/dandisets_linkml_status/cli/__init__.py
+++ src/dandisets_linkml_status/cli/__init__.py
@@ -1,8 +1,24 @@
-from typer import Typer
+import logging
+from typing import Annotated
 
-app = Typer()
+import typer
+from dandi.dandiapi import DandiAPIClient
+from pydantic2linkml.cli.tools import LogLevel
+
+logger = logging.getLogger(__name__)
+app = typer.Typer()
 
 
 @app.command()
-def main():
-    print("Hello World")
+def main(
+    *,
+    include_unpublished: Annotated[
+        bool, typer.Option("--include-unpublished", "-u")
+    ] = False,
+    log_level: Annotated[
+        LogLevel, typer.Option("--log-level", "-l")
+    ] = LogLevel.WARNING,
+):
+    # Set log level of the CLI
+    logging.basicConfig(level=getattr(logging, log_level))
+
