bb85f54d333eb37dd878f4c7ec6733ced360efb1
Isaac To <isaac.chun.to@gmail.com>
2024-08-14 14:59:00 -0700

    Define `cli.models.DandisetValidationReport`

---
+++ src/dandisets_linkml_status/cli/models.py
@@ -0,0 +1,20 @@
+from typing import Any, Optional
+
+from linkml.validator.report import ValidationResult
+from pydantic import BaseModel, Json
+
+
+class DandisetValidationReport(BaseModel):
+    """
+    A report of validation results of a dandiset (metadata) against the
+    `dandischema.models.Dandiset` Pydantic model and the corresponding LinkML schema.
+    """
+
+    dandiset_identifier: str
+    dandiset_metadata: dict[str, Any]  # The metadata of the dandiset to be validated
+
+    # Error encountered in validation against the Pydantic dandiset metadata model
+    pydantic_validation_errors: Optional[Json[list[dict[str, Any]]]] = None
+
+    # Errors encountered in validation against the dandiset metadata model in LinkML
+    linkml_validation_errors: Optional[list[ValidationResult]] = None
