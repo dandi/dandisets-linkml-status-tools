f91d008a053f73859fc2118c4231fe38a47ac74c
Isaac To <isaac.chun.to@gmail.com>
2024-08-14 19:07:27 -0700

    Rename model fields to shorter versions

--- src/dandisets_linkml_status/cli/models.py
+++ src/dandisets_linkml_status/cli/models.py
@@ -14,7 +14,7 @@ class DandisetValidationReport(BaseModel):
     dandiset_metadata: dict[str, Any]  # The metadata of the dandiset to be validated
 
     # Error encountered in validation against the Pydantic dandiset metadata model
-    pydantic_validation_errors: Optional[Json[list[dict[str, Any]]]] = None
+    pydantic_validation_errs: Optional[Json[list[dict[str, Any]]]] = None
 
     # Errors encountered in validation against the dandiset metadata model in LinkML
-    linkml_validation_errors: Optional[list[ValidationResult]] = None
+    linkml_validation_errs: Optional[list[ValidationResult]] = None
