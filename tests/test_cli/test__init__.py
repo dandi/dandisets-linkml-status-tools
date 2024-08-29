31fbdfd676d365269593badb1cd7d709ecc34318
Isaac To <isaac.chun.to@gmail.com>
2024-08-28 17:38:10 -0700

    Define a smoke test of the CLI

---
+++ tests/test_cli/test__init__.py
@@ -0,0 +1,17 @@
+import pytest
+from typer.testing import CliRunner
+
+from dandisets_linkml_status.cli import app
+
+runner = CliRunner()
+
+
+@pytest.mark.skip(
+    reason="This test currently takes too long. It should be re-enabled "
+    "when there is an option in the app to limit the number of "
+    "dandisets to validate."
+)
+def test_smoke_cli(tmp_path):
+    validation_report_file_path = tmp_path / "validation_reports.json"
+    result = runner.invoke(app, ["--output-file", str(validation_report_file_path)])
+    assert result.exit_code == 0
