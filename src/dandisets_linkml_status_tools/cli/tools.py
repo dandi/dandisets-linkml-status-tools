a3a81a0c798c6c20275057b4201dec37e9c1f42a
Isaac To <isaac.chun.to@gmail.com>
2024-10-06 18:52:36 -0700

    Rename `DANDI_LINKML_SCHEMA` to `_DANDI_LINKML_SCHEMA`

--- src/dandisets_linkml_status_tools/cli/tools.py
+++ src/dandisets_linkml_status_tools/cli/tools.py
@@ -39,7 +39,7 @@ logger = logging.getLogger(__name__)
 DANDI_MODULE_NAMES = ["dandischema.models"]
 
 # The LinkML schema produced by the pydantic2linkml translator for DANDI models
-DANDI_LINKML_SCHEMA = translate_defs(DANDI_MODULE_NAMES)
+_DANDI_LINKML_SCHEMA = translate_defs(DANDI_MODULE_NAMES)
 
 # A callable that sorts a given iterable of strings in a case-insensitive manner
 isorted = partial(sorted, key=str.casefold)
@@ -84,7 +84,7 @@ class DandisetLinkmlValidator:
             validation_plugins = [JsonschemaValidationPlugin(closed=True)]
 
         self._inner_validator = Validator(
-            DANDI_LINKML_SCHEMA,
+            _DANDI_LINKML_SCHEMA,
             validation_plugins=validation_plugins,
         )
 
