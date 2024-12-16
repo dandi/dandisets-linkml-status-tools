5e8016dbee91e5158c5d87ec26a82ff0f5ba43d5
Isaac To <isaac.chun.to@gmail.com>
2024-12-16 09:55:54 -0800

    feat: define models for representing diff reports

--- src/dandisets_linkml_status_tools/cmd_funcs/diff_manifests_reports.py
+++ src/dandisets_linkml_status_tools/cmd_funcs/diff_manifests_reports.py
@@ -1,5 +1,8 @@
 import logging
 from pathlib import Path
+from typing import Annotated, Any
+
+from pydantic import Field
 
 from dandisets_linkml_status_tools.cli import (
     ASSET_VALIDATION_REPORTS_FILE,
@@ -9,11 +12,42 @@ from dandisets_linkml_status_tools.models import (
     ASSET_VALIDATION_REPORTS_ADAPTER,
     DANDISET_VALIDATION_REPORTS_ADAPTER,
     AssetValidationReportsType,
+    DandiBaseReport,
     DandisetValidationReportsType,
+    PydanticValidationErrsType,
 )
 from dandisets_linkml_status_tools.tools import read_reports
 
 logger = logging.getLogger(__name__)
+
+
+class _DandiValidationDiffReport(DandiBaseReport):
+    """
+    A base class for DANDI validation diff reports
+    """
+
+    pydantic_validation_errs1: Annotated[
+        PydanticValidationErrsType, Field(default_factory=list)
+    ]
+    pydantic_validation_errs2: Annotated[
+        PydanticValidationErrsType, Field(default_factory=list)
+    ]
+    pydantic_validation_errs_diff: Any
+
+
+class _DandisetValidationDiffReport(_DandiValidationDiffReport):
+    """
+    A class for Dandiset validation diff reports
+    """
+
+
+class _AssetValidationDiffReport(_DandiValidationDiffReport):
+    """
+    A class for Asset validation diff reports
+    """
+
+    asset_id: str | None
+    asset_path: str | None
 
 
 def diff_manifests_reports(
