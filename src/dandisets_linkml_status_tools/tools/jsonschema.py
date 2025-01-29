b2286c26b32343bc4554b99291a24d902cf253e1
Isaac To <isaac.chun.to@gmail.com>
2025-01-29 11:45:09 -0800

    feat: allow `jsonschema_validator()` to provide a default validator cls

--- src/dandisets_linkml_status_tools/tools/jsonschema.py
+++ src/dandisets_linkml_status_tools/tools/jsonschema.py
@@ -8,8 +8,13 @@ from jsonschema.validators import validator_for
 from dandisets_linkml_status_tools.models import JsonschemaValidationErrorModel
 
 
+# todo: this function is available through the dandischema package if
+#  https://github.com/dandi/dandi-schema/pull/278 is accepted
 def jsonschema_validator(
-    schema: dict[str, Any], *, check_format: bool
+    schema: dict[str, Any],
+    *,
+    check_format: bool,
+    default_cls: type[JsonschemaValidator] | None = None,
 ) -> JsonschemaValidator:
     """
     Create a JSON schema validator appropriate for validating instances against a given
@@ -18,10 +23,19 @@ def jsonschema_validator(
     :param schema: The JSON schema to validate against
     :param check_format: Indicates whether to check the format against format
         specifications in the schema
+    :param default_cls: The default JSON schema validator class to use to create the
+        validator should the appropriate validator class cannot be determined based on
+        the schema (by assessing the `$schema` property). If `None`, the class
+        representing the latest JSON schema draft supported by the `jsonschema` package.
     :return: The JSON schema validator
+    :raises jsonschema.exceptions.SchemaError: If the JSON schema is invalid
     """
     # Retrieve appropriate validator class for validating the given schema
-    validator_cls = validator_for(schema)
+    validator_cls: type[JsonschemaValidator] = (
+        validator_for(schema, default_cls)
+        if default_cls is not None
+        else validator_for(schema)
+    )
 
     # Ensure the schema is valid
     validator_cls.check_schema(schema)
